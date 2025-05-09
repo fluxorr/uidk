536e8af363f3935a7d7c7f3d3946c8fdb3f7acf4
Rahul Chaudhari_ <rahulgc545@gmail.com>
2025-05-09 15:09:57 +0530

    better homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div className="text-white text-6xl "  >
-            Hey this is home!
+        <div className="text-[#553b08] flex flex-col justify-center items-center h-screen bg-[#e9e5cd] text-6xl">
+            <div>Welcome to Rahuls's Ui Recreation Space</div>
+            <div className="grid grid-cols-1" >
+                <Link to="/uidk/design1" className="text-2xl mt-4  hover:text-[#75420e] hover:scale-99  underline">
+                    Go to Design 1
+                </Link>
+                <Link to="/uidk/design2" className="text-2xl mt-4 flex text-center items-center hover:text-[#75420e] hover:scale-99  underline">
+                    Go to Design 2 <div className="text-[10px]"  > &nbsp; (incomplete)</div>
+                </Link>
+
+            </div>
         </div>
     );
 }
