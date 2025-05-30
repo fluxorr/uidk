ab1b9e40d6f6713e3f3d8316e43a7e45fd6ba02c
Rahul Chaudhari_ <rahulgc545@gmail.com>
2025-05-30 23:38:23 +0530

    landing page

---
+++ src/components/design4/BgCircle.tsx
@@ -0,0 +1,13 @@
+
+
+function BgCircle() {
+    return (
+        <div className="absolute mt-64 w-[825px] h-[825px] l-[167px] top-[260px]  bg-[#602A9A] blur-[267px] " >
+
+            <div className="  absolute w-[569px] h-[569px] left-[295px] top-[388px]  bg-[#622A9A] blur-[112px] " ></div >
+        </div >
+
+    )
+}
+
+export default BgCircle
--- src/components/design4/Home.tsx
+++ src/components/design4/Home.tsx
@@ -1,23 +1,32 @@
+import BgCircle from "./BgCircle"
+import Mock3 from "../../assets/mock3.png"
 
 
 function Home() {
     return (
-        <div className="flex flex-col justify-center items-center mt-32  " >
-            <div className="rounded-full flex items-center border-1 border-neutral-100/15 w-fit px-3 py-2 gap-2" >
+        <div className="flex flex-col justify-center items-center mt-32 z-[999] "  >
+            <div style={{ zIndex: "100" }} className="rounded-full flex items-center border-1 border-neutral-100/15 w-fit px-3 py-2 gap-2" >
                 <div className="bg-[#9855FF] h-[18px] text-center flex items-center font-semibold w-fit rounded-full px-1 text-sm py-1.5 " >NEW</div>
                 <div className="text-[#9855FF]" >Latest integration just arrived</div>
             </div>
 
 
-            <div className=" font-medium text-[102px] leading-[94px] text-center tracking-[-0.0506em] bg-gradient-to-b from-white to-[#B372CF] bg-clip-text text-transparent z-0 pb-4" >
+            <div style={{ zIndex: "100" }} className=" font-medium text-[102px] leading-[94px] text-center tracking-[-0.0506em] bg-gradient-to-b from-white to-[#B372CF] bg-clip-text text-transparent z-0 pb-4" >
                 <div>Boost Your </div>
                 <div>rankings with AI.</div>
             </div>
 
-            <div className="text-white/80 text-[20px] items-center flex justify-center flex-col">
+            <div style={{ zIndex: "100" }} className="text-white/80 text-[20px] items-center flex justify-center flex-col">
                 <div>Elevate your site’s visibility effortlessly with AI,</div> <div>where smart technology meets user-friendly SEO tools.</div>
             </div>
-            <button className="border-1  text-black/60 mt-8 hover:scale-101 transition-transform duration-300 border-neutral-100/40 w-fit h-fit px-2 py-2 rounded-[14px] cursor-pointer" ><div className=" px-4 py-2 border-1 border-neutral-100/40  inset-0  bg-white  rounded-[10px] backdrop-blur-xs" >Start for free</div></button>
+            <button style={{ zIndex: "100" }} className="border-1  text-black/90 mt-8 hover:scale-101 transition-transform duration-300 border-neutral-100/40 w-fit h-fit px-2 py-2 rounded-[14px] cursor-pointer" ><div className=" px-4 py-2 border-1 border-neutral-100/40  inset-0  bg-white  rounded-[10px] backdrop-blur-xs z-50 cursor-pointer " >Start for free</div></button>
+
+            <BgCircle />
+            <div className="rounded-[100%] border-1 border-neutral-100/4 h-[1032px] w-[1032px] absolute " style={{ zIndex: "90" }} ></div>
+
+            <div className="rounded-[100%] border-3 border-dashed border-neutral-100/3 h-[800px] w-[800px] absolute " style={{ zIndex: "90" }} ></div>
+            <div className="rounded-[100%] border-1  border-neutral-100/4 h-[565px] w-[565px] absolute " style={{ zIndex: "90" }} ></div>
+            <img src={Mock3} alt="mock image of platform" style={{ zIndex: "100" }} />
 
         </div >
     )
